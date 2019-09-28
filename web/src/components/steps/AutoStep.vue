bf30b8c343c509649ee39e5b1a742e6cc13cd3f9
Alexey Khrushch <alexeykhr@outlook.com>
2019-09-29 00:08:41 +0300

    perf: html nesting reduced

--- web/src/components/steps/AutoStep.vue
+++ web/src/components/steps/AutoStep.vue
@@ -1,36 +1,49 @@
 <template>
   <step :menu="menu">
-    <auto-step-item
+    <block
       v-for="(item, index) in activeMenu"
       :key="index"
-      :item="item"
-    />
+      :title="item.title"
+      :href="item.href"
+      :level="item.level"
+    >
+      <component :is="item.component" />
+    </block>
   </step>
 </template>
 
 <script>
-import AutoStepItem from '@/components/steps/AutoStepItem'
+import Block from '@/components/steps/Block'
 import Step from '@/components/steps/Step'
 
 export default {
   components: {
-    Step, AutoStepItem
+    Step, Block
   },
   props: {
     menu: {
       type: Array,
-      required: () => []
+      required: true
     }
   },
   computed: {
     activeMenu() {
-      return this.menu.filter((item) => {
-        if (typeof item.enable === 'undefined') {
-          return true
+      const list = []
+
+      const each = (menu, level = 1) => {
+        if (typeof menu.enable === 'undefined' || menu.enable) {
+          menu.level = level
+          list.push(menu)
         }
 
-        return item.enable
-      })
+        if (Array.isArray(menu.children)) {
+          menu.children.forEach((item) => each(item, level + 1))
+        }
+      }
+
+      this.menu.forEach((item) => each(item))
+
+      return list
     }
   }
 }
