ab3cd4e986f81739f94c02f9761fd85e6dd9f882
Alexey Khrushch <alexeykhr@outlook.com>
2019-12-10 23:21:08 +0200

    fix: random on start application

--- services/helpers/str.go
+++ services/helpers/str.go
@@ -1,9 +1,14 @@
 package helpers
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 // Rnd generate a random of symbols specified length
 func Rnd(n int) string {
+	rand.Seed(time.Now().UnixNano())
+
 	letterRunes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	letterRunesLen := len(letterRunes)
 	b := make([]rune, n)
