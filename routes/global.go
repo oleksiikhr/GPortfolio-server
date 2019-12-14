d72e0fceff6fd07175d1fe74f66da33ffc3363d8
Alexey Khrushch <alexeykhr@outlook.com>
2019-12-14 21:00:48 +0200

    chorr(http/2): remove push, only static server

--- routes/global.go
+++ routes/global.go
@@ -1,28 +1,10 @@
 package routes
 
 import (
-	"io/ioutil"
 	"net/http"
-
-	"github.com/GPortfolio/server/config"
 )
 
 // globalRoutes main routes for Frontend parts
 func (h *Handlers) globalRoutes() {
-	http.Handle("/static/", http.StripPrefix("/", http.FileServer(http.Dir("dist"))))
-	http.HandleFunc("/", h.handleMain())
-}
-
-// handleMain handler for displaying the main page
-func (h *Handlers) handleMain() http.HandlerFunc {
-	html, err := ioutil.ReadFile(config.HomePageFile)
-	if err != nil {
-		h.Logger.Fatal(err)
-	}
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		push(w, "/static/main.js", "/static/main.css")
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.Write(html)
-	}
+	http.Handle("/", http.FileServer(http.Dir("dist")))
 }
--- routes/routes.go
+++ routes/routes.go
@@ -32,15 +32,6 @@ func (h *Handlers) tryAccessToken(r *http.Request) interface{} {
 	return h.Redis.SecGetHard(key, pass)
 }
 
-// push to speed up content delivery (http/2)
-func push(w http.ResponseWriter, resources ...string) {
-	if pusher, ok := w.(http.Pusher); ok {
-		for _, resource := range resources {
-			_ = pusher.Push(resource, nil)
-		}
-	}
-}
-
 // response json by specific structure
 func response(w http.ResponseWriter, msg interface{}, statusCode int) map[string]interface{} {
 	w.Header().Set("Content-Type", "application/json")
